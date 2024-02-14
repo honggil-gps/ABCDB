d96445dfb0eb664de42fec485ab18df733434d7f
Park HyeonJun <baghyeonjun16@gmail.com>
2024-02-14 16:16:59 +0900

    setting_PHJ

---
+++ connect.js
@@ -0,0 +1,17 @@
+let mysql = require('mysql');
+
+// process.env.를 이용한 .env파일 접근은 Node.js v20.6.0부터 사용가능
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) return console.error(err.message);
+
+  console.log('Connected to the MySQL server.');
+});
+
