3ffbfbbb74989117dd87b9c3f675a3da821be746
unknown <dsmamkm@gmail.com>
2024-02-15 18:35:58 +0900

    program_update구현_min

--- update/update.js
+++ update/update.js
@@ -1,38 +1,30 @@
-// let mysql = require('mysql');
+let mysql = require('mysql');
 
-// let connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+function update(id, change ,table, attr){
+connection.connect((err) => {
+  if (err) return console.error(err.message);
 
-// connection.connect((err) => {
-//   if (err) return console.error(err.message);
+  let sql = `UPDATE  ${table}
+    SET ${attr} = ?
+    WHERE userid = ?`;
 
-//   let sql = `UPDATE  user
-//     SET completed = ?
-//     WHERE id = ?`;
+  let data = [change, id];
 
-//   let data = [false, 1];
+  connection.query(sql, data, (error, results, fields) => {
+    if (error) return console.error(error.message);
+    console.log('Rows affected:', results.affectedRows);
+  });
 
-//   connection.query(sql, data, (error, results, fields) => {
-//     if (error) return console.error(error.message);
-//     console.log('Rows affected:', results.affectedRows);
-//   });
+  // close the database connection
+  connection.end();
 
-//   // close the database connection
-//   connection.end();
-// });
-
-// // // 오류가 발생한 경우
-// // connection.connect((err)=>{
-// //   if(err) return console.log(err.message);
-// //   console.log('연결 성공');
-// // })
-
-// // connection.end((err)=>{
-// //   if(err) return console.error(err.message);
-// //   console.log('연결 해제')
-// // })
+});
+}
+module.exports = {update}
